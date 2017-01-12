2e675eb9213f2dccc4918fea98c853509ac4c692
周茜(xn056839) <zhouxi2@xiaoniu66.com>
2017-01-12 19:55:32 +0800

    1.12

--- manage_platform/src/main/java/com/xn/manage/bean/KeyValueStore.java
+++ manage_platform/src/main/java/com/xn/manage/bean/KeyValueStore.java
@@ -25,6 +25,8 @@ public class KeyValueStore {
 	public String toString() {
 		return "KeyValueStore [name=" + name + ", value=" + value + "]";
 	}
+
+
     
     
 }
--- manage_platform/src/main/java/com/xn/manage/controller/CaseController.java
+++ manage_platform/src/main/java/com/xn/manage/controller/CaseController.java
@@ -1,16 +1,22 @@
 package com.xn.manage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/case")
 public class CaseController {
 	
 	@RequestMapping(value="/{path}", method = RequestMethod.GET)
-	public String getCasePage(@PathVariable String  path) {
+	public String getCasePage(@PathVariable String  path, HttpServletRequest request, ModelMap map) {
+		String type=request.getParameter("caseType");
+
+		map.put("caseType",type);
 		return "case/" + path;
 	}
 
