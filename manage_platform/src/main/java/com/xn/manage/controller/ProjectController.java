26a0da7e7d605264d195a82eaafb9666cd673beb
xn058121 <gaoshan8023@qq.com>
2017-01-17 14:58:02 +0800

    修改测试集

--- manage_platform/src/main/java/com/xn/manage/controller/ProjectController.java
+++ manage_platform/src/main/java/com/xn/manage/controller/ProjectController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/company")
-public class CompanyController {
+@RequestMapping("/project")
+public class ProjectController {
 	
 	@RequestMapping(value="/{path}", method = RequestMethod.GET)
 	public String getSystemPage(@PathVariable String  path) {
-		return "company/" + path;
+		return "project/" + path;
 	}
 
 }
