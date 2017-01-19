bc25f39d865ae376d196d5599e996a3afa5eefa6
xn058121 <gaoshan8023@qq.com>
2017-01-19 14:48:42 +0800

    优化

--- manage_platform/src/main/java/com/xn/manage/controller/InterfaceController.java
+++ manage_platform/src/main/java/com/xn/manage/controller/InterfaceController.java
@@ -1,9 +1,16 @@
 package com.xn.manage.controller;
 
+import com.xn.manage.Enum.ContentTypeEnum;
+import com.xn.manage.Enum.HttpTypeEnum;
+import com.xn.manage.Enum.RequestTypeEnum;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/interface")
@@ -14,7 +21,24 @@ public class InterfaceController {
 	}
 
 	@RequestMapping(value="/{path}", method = RequestMethod.GET)
-	public String getPlanPage(@PathVariable String  path) {
+	public String getPlanPage(@PathVariable String  path ,ModelMap map) {
+		List<ContentTypeEnum> contentTypeList = new ArrayList<ContentTypeEnum>();
+		for(ContentTypeEnum item : ContentTypeEnum.values()){
+			contentTypeList.add(item);
+		}
+
+		List<HttpTypeEnum> httpTypeList = new ArrayList<HttpTypeEnum>();
+		for(HttpTypeEnum item : HttpTypeEnum.values()){
+			httpTypeList.add(item);
+		}
+
+		List<RequestTypeEnum> requestTypeList = new ArrayList<RequestTypeEnum>();
+		for(RequestTypeEnum item : RequestTypeEnum.values()){
+			requestTypeList.add(item);
+		}
+		map.put("requestTypeList",requestTypeList);
+		map.put("contentTypeList",contentTypeList);
+		map.put("httpTypeList",httpTypeList);
 		return "interface/" + path;
 	}
 
