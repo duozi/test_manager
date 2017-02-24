18d9ecd426ce084b78f018dd8415addf203dc95b
周茜(xn056839) <zhouxi2@xiaoniu66.com>
2017-02-24 10:10:08 +0800

    2.24

--- manage_platform/src/main/java/com/xn/manage/performanceController/PerformanceMachineController.java
+++ manage_platform/src/main/java/com/xn/manage/performanceController/PerformanceMachineController.java
@@ -11,12 +11,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 @RequestMapping("/performance/machine")
@@ -28,16 +26,21 @@ public class PerformanceMachineController {
 
     @RequestMapping(value = "/{path}", method = RequestMethod.GET)
     public String common(@PathVariable String path, ModelMap model) {
+        if (path.equals("stress_machine_list")) {
+            List<PerformanceStressMachineDto> performanceStressMachineDtoList = performanceStressMachineService.list(new PerformanceStressMachineDto());
+            model.put("stress_machine_list", performanceStressMachineDtoList);
+        } else if (path.equals("monitored_machine_list")) {
 
+        }
         return "/performance/machine/" + path;
     }
 
     @RequestMapping(value = "/stress_machine_list/save", method = RequestMethod.POST)
     @ResponseBody
     public CommonResult saveStressMachine(PerformanceStressMachineDto performanceStressMachineDto) {
-        CommonResult commonResult=new CommonResult();
+        CommonResult commonResult = new CommonResult();
         if (!ValidateUtil.validate(performanceStressMachineDto)) {
-            logger.warn(String.format("参数有误",performanceStressMachineDto));
+            logger.warn(String.format("参数有误", performanceStressMachineDto));
             commonResult.setCode(CommonResultEnum.PARAM_ERROR.getReturnCode());
             commonResult.setMessage(CommonResultEnum.PARAM_ERROR.getReturnMsg());
             return commonResult;
@@ -45,6 +48,23 @@ public class PerformanceMachineController {
         performanceStressMachineService.save(performanceStressMachineDto);
         commonResult.setCode(CommonResultEnum.SUCCESS.getReturnCode());
         commonResult.setMessage(CommonResultEnum.SUCCESS.getReturnMsg());
+        return commonResult;
+    }
+
+    @RequestMapping(value = "/stress_machine_list/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult deleteStressMachine(@RequestParam Integer id) {
+        CommonResult commonResult = new CommonResult();
+        PerformanceStressMachineDto performanceStressMachineDto = new PerformanceStressMachineDto();
+        performanceStressMachineDto.setId(id);
+        int n = performanceStressMachineService.delete(performanceStressMachineDto);
+        if (n == 0) {
+            commonResult.setCode(CommonResultEnum.ERROR.getReturnCode());
+            commonResult.setMessage(CommonResultEnum.ERROR.getReturnMsg());
+        } else {
+            commonResult.setCode(CommonResultEnum.SUCCESS.getReturnCode());
+            commonResult.setMessage(CommonResultEnum.SUCCESS.getReturnMsg());
+        }
         return commonResult;
     }
 
