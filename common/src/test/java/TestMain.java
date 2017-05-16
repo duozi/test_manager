a50fdb5944c0beba6c2685b46a487848d7a47345
高珊(xn058121) <gaoshan@xiaoniu66.com>
2017-05-16 11:26:33 +0800

    合并后提交

--- common/src/test/java/TestMain.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by xn058121 on 2017/2/16.
- */
-
-public class TestMain {
-    public static void main(String[] args) throws Exception {
-        com.alibaba.dubbo.container.Main.main(args);
-    }
-}
