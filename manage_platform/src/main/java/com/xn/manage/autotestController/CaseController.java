5619f29ffe7f1014fe09f8ac7a50647cadd0a4c0
高珊(xn058121) <gaoshan@xiaoniu66.com>
2017-05-17 15:46:21 +0800

    tijiao

--- manage_platform/src/main/java/com/xn/manage/autotestController/CaseController.java
+++ manage_platform/src/main/java/com/xn/manage/autotestController/CaseController.java
@@ -10,6 +10,7 @@ import com.xn.interfacetest.api.*;
 import com.xn.interfacetest.dto.*;
 import com.xn.manage.Enum.ParamTypeEnum;
 import com.xn.manage.utils.JsonValidator;
+import com.xn.manage.utils.ModelUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
